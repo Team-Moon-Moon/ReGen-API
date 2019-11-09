7238e4c68adf37c05fcc2597f02a99e8d7ebf17b
Kyle Wu <kylewu2@gmail.com>
2019-11-08 16:28:38 -0800

    Commented out obsolete fields to avoid deserializing them

--- FirebaseAuthDemo/Data Models/Recipe.cs
+++ FirebaseAuthDemo/Data Models/Recipe.cs
@@ -38,13 +38,13 @@ namespace FirebaseAuthDemo.Models
         public IEnumerable<string> Steps
         { get; set; }
 
-        [Obsolete]
-        public int StarRating
-        { get; set; }
+        //[Obsolete]
+        //public int StarRating
+        //{ get; set; }
 
-        [Obsolete]
-        public string[] Reviews
-        { get; set; }
+        //[Obsolete]
+        //public string[] Reviews
+        //{ get; set; }
 
         public IEnumerable<string> Tags
         { get; set; }
@@ -58,39 +58,6 @@ namespace FirebaseAuthDemo.Models
         { get; set; }
 
         #endregion
-
-        ///// <summary>
-        ///// Creates a new instance of Recipe
-        ///// </summary>
-        ///// <param name="name">The name of the recipe</param>
-        ///// <param name="imagePath">Image itemPath url for the database</param>
-        ///// <param name="calories">The amount of calories</param>
-        ///// <param name="prepTimeMinutes">The prep time in minutes</param>
-        ///// <param name="tags">The tags for this recipe</param>
-        ///// <param name="ingredients">The ingredients for this recipe</param>
-        ///// <param name="steps">The steps for this recipe</param>
-        ///// <param name="reviews">The reviews for this recipe</param>
-        ///// <param name="starRating">The rating for this recipe</param>
-        //public Recipe(string name, string imagePath, int calories, int prepTimeMinutes, List<string> tags, List<Ingredient> ingredients, List<string> steps, List<string> reviews = null, int starRating = 5, string key = "")
-        //{
-
-        //    Name = name;
-        //    ImageReferencePath = imagePath;
-        //    Calories = calories;
-        //    PrepTimeMinutes = prepTimeMinutes;
-        //    Tags = tags.ToArray();
-        //    Ingredients = ingredients.ToArray();
-        //    Steps = steps.ToArray();
-        //    StarRating = starRating;
-
-        //    if (reviews != null)
-        //        Reviews = reviews.ToArray();
-
-        //    Key = key;
-
-
-        //}
-
         
         public override string ToString()
         {
